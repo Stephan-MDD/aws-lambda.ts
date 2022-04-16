fe1032e1471ba8befaed03a19abe0d10008441a4
Stephan Djurhuus <stephan.mdd@gmail.com>
2022-04-16 23:02:22 +0200

    fixed module issue

--- src/functions/todo.functions.ts
+++ src/functions/todo.functions.ts
@@ -5,31 +5,31 @@ type ASWFunctions = AWS['functions'];
 export const todoFunctions: ASWFunctions = {
 	queryTodo: {
 		name: 'query-todo',
-		handler: 'build/src/functions/todo.handlers.query',
+		handler: 'build/src/functions/todo.query',
 		events: [{ http: { method: 'get', path: 'todo', cors: true } }],
 	},
 
 	getTodo: {
 		name: 'get-todo',
-		handler: 'build/src/functions/todo.handlers.get',
+		handler: 'build/src/functions/todo.get',
 		events: [{ http: { method: 'get', path: 'todo/{id}', cors: true } }],
 	},
 
 	createTodo: {
 		name: 'create-todo',
-		handler: 'build/src/functions/todo.handlers.create',
+		handler: 'build/src/functions/todo.create',
 		events: [{ http: { method: 'post', path: 'todo', cors: true } }],
 	},
 
 	updateTodo: {
 		name: 'update-todo',
-		handler: 'build/src/functions/todo.handlers.update',
+		handler: 'build/src/functions/todo.update',
 		events: [{ http: { method: 'put', path: 'todo/{id}', cors: true } }],
 	},
 
 	removeTodo: {
 		name: 'remove-todo',
-		handler: 'build/src/functions/todo.handlers.remove',
+		handler: 'build/src/functions/todo.remove',
 		events: [{ http: { method: 'delete', path: 'todo/{id}', cors: true } }],
 	},
 };
